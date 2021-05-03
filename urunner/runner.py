12ddac9b0a1caee9aa657cfd2fb1c49813694ff2
Remaetanju <remibraizet@gmail.com>
2021-05-03 18:04:48 +0200

    first commit

---
+++ urunner/runner.py
@@ -0,0 +1,25 @@
+from user_code.uparser import run
+import datetime
+import logging
+import sys
+
+
+class Urunner:
+    def __init__(self):
+        self.start_time = datetime.datetime.utcnow()
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        logging.info("test started: {}".format(datetime.datetime.utcnow()))
+        self.check_parser_syntax()
+
+    def __del__(self):
+        self.end_time = datetime.datetime.utcnow()
+        logging.info("test ended: {}".format(datetime.datetime.utcnow()))
+
+        self.run_time = self.end_time - self.start_time
+        logging.info("test tun time: {}".format(self.run_time))
+
+    def check_parser_syntax(self):
+        print("running uparser: {}".format(run()))
+
+
+test = Urunner()
