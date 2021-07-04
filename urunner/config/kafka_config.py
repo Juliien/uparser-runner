04c1757cf1cdd89f2c79d47d74732380825171a9
Juliien <julien1105@outlook.fr>
2021-07-04 19:13:34 +0200

    fix: upadate config & requirements.txt

--- urunner/config/kafka_config.py
+++ urunner/config/kafka_config.py
@@ -1,6 +1,6 @@
 cfg = {'input': {'topic': 'runner-input',
                  'server': "localhost:9092"},
-       'output': {'topic': 'runner-ouput',
+       'output': {'topic': 'runner-output',
                   'server': 'localhost:9092'}}
 
 "/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic runner-output"
