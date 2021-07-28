2f6c88d3b38d6b3f6ad2d85d9a012bc3bba5f909
Remi <remi@harfanglab.fr>
2021-07-28 22:07:16 +0200

    kafka configuration with env variables

--- urunner/config/kafka_config.py
+++
@@ -1,4 +0,0 @@
-cfg = {'input': {'topic': 'runner-input',
-                 'server': "localhost:9092"},
-       'output': {'topic': 'runner-output',
-                  'server': 'localhost:9092'}}
--- urunner/kafka_wrapper.py
+++ urunner/kafka_wrapper.py
@@ -1,12 +1,17 @@
+import os
 import json
 import logging
 
 from kafka import KafkaConsumer, KafkaProducer
-from config.kafka_config import cfg
+
+# kafka default config, you can override this by setting INPUT_SERVER, INPUT_TOPIC and OUTPUT_SERVER to your environment
+cfg = {'input': {'topic': 'runner-input', 'server': "localhost:9092"},
+       'output': {'topic': 'runner-output', 'server': 'localhost:9092'}}
 
 
 class Consumer:
     def __init__(self, logger=None):
+
         if logger:
             self.Logger = logger
         else:
@@ -14,13 +19,23 @@ class Consumer:
             self.Logger.setLevel(logging.INFO)
 
         self.Logger.info("kafka consumer setup start")
-        self.input = cfg['input']
+
+        if "INPUT_SERVER" in os.environ:
+            self.input_server = os.environ['INPUT_SERVER']
+        else:
+            self.input_server = cfg['input']['server']
+
+        if 'INPUT_TOPIC' in os.environ:
+            self.input_topic = os.environ['INPUT_TOPIC']
+        else:
+            self.input_topic = cfg['input']['topic']
+
         self.consumer = ""
         self.setup_kafka_consumer()
         self.Logger.info("kafka consumer setup end")
 
     def setup_kafka_consumer(self):
-        self.consumer = KafkaConsumer(self.input['topic'], bootstrap_servers=[self.input['server']],
+        self.consumer = KafkaConsumer(self.input_topic, bootstrap_servers=[self.input_server],
                                       auto_offset_reset='earliest',
                                       value_deserializer=lambda x: json.loads(x.decode('utf-8')), group_id='urunner-0')
         KafkaConsumer(consumer_timeout_ms=1000)
@@ -36,15 +51,19 @@ class Producer:
         else:
             self.Logger = logging.getLogger()
             self.Logger.setLevel(logging.INFO)
-
         self.Logger.info("kafka producer setup start")
-        self.output = cfg['output']
+
+        if 'OUTPUT_SERVER' in os.environ:
+            self.output_server = os.environ['OUTPUT_SERVER']
+        else:
+            self.output_server = cfg['output']['server']
+
         self.producer = ""
         self.setup_kafka_producer()
         self.Logger.info("kafka producer setup end")
 
     def setup_kafka_producer(self):
-        self.producer = KafkaProducer(bootstrap_servers=[self.output['server']],
+        self.producer = KafkaProducer(bootstrap_servers=[self.output_server],
                                       value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
     def __del__(self):
